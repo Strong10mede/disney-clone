3e2abe4a2c1ba4c43788d7001ceba82465057ff7
Strong10mede <kmayur809@gmail.com>
2022-12-25 13:39:47 +0530

    userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -30,3 +30,5 @@ export const { setUserLoginDetails, setSignOutState } = userSlice.actions;
 export const selectUserName = (state) => state.user.name;
 export const selectUserEmail = (state) => state.user.email;
 export const selectUserPhoto = (state) => state.user.photo;
+
+export default userSlice.reducer;
