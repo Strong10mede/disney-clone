e940e43cb285d8719f56d78e65d637d5c8c481d0
Strong10mede <kmayur809@gmail.com>
2022-12-26 12:49:08 +0530

    NewDisney.js done

--- src/Components/NewDisney.js
+++ src/Components/NewDisney.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectNewDisney } from "../features/movies/movieSlice";
+
 function NewDisney() {
+  const movies = useSelector(selectNewDisney);
+  console.log(movies);
   return (
     <Container>
-      <h4>Reommended for You</h4>
-      <Content></Content>
+      <h4>New Disney Special</h4>
+
+      <Content>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={`/detail/` + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
+      </Content>
     </Container>
   );
 }
