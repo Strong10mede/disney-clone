0f77003abfdd69d1a28fd03f7ce9544e0ef6bdb0
Strong10mede <kmayur809@gmail.com>
2022-12-26 10:59:20 +0530

    movieReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
-//userReducer is the single root reducer function
+import movieReducer from "../features/movies/movieSlice";
+//reducer is the single root reducer function
 //configure store is helping to create the single store
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
