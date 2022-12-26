2521efc2cd49ee04be55b586c81db2da96fea893
Strong10mede <kmayur809@gmail.com>
2022-12-26 09:30:00 +0530

    all needed files creation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
 function App() {
   return (
     <div className="app">
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
